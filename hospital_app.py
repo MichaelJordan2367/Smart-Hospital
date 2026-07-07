aa4f98ec3e4619be12de954a8c48f2f16b486d04
MichaelJordan2367 <jordanabrahamn@gmail.com>
2026-07-07 10:36:45 +0700

    Fix file opening method for hospital model

--- hospital_app.py
+++ hospital_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 import pandas as pd
 import pickle
 
-with oper("hospital_model.pkl") as f:
+with open("hospital_model.pkl") as f:
   bundle = pickle.load(f)
   st.write("conected") 
